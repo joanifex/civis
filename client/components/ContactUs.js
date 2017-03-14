12ce835da492e95c1ba2f984937c75604701fc45
Valerie Barela <vbarela1@gmail.com>
2017-03-14 15:37:44 -0600

    creates static pages and mobile nav bar

---
+++ client/components/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactUs extends React.Component{
+  render (){
+    return(
+      <div className='center'>
+        Contact Us
+      </div>
+    );
+  }
+}
+
+export default ContactUs;
--- client/routes.js
+++ client/routes.js
@@ -5,6 +5,10 @@ import NoMatch from './components/NoMatch';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import { UserAuthWrapper } from 'redux-auth-wrapper';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
+
 
 const UserIsAuthenticated = UserAuthWrapper({
   authSelector: state => state.auth,
@@ -15,8 +19,11 @@ export default (
   <Route>
 
     <Route path="/" component={App}>
+      <IndexRoute component={Home} />
       <Route path='/login' component={Login} />
       <Route path='/sign_up' component={SignUp} />
+      <Route path='/about_us' component={AboutUs} />
+      <Route path='/contact_us' component={ContactUs} />
     </Route>
 
     <Route path="*" status={404} component={NoMatch} />
