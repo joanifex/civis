3ae5e3b70394796b4fa9ecef2daa9d229f5aa669
Ian Wright <wright.ianb@gmail.com>
2017-03-15 15:31:59 -0600

    fixes user signup, changes user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,9 +25,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :first_name, :last_name, :zipcode
   validates_format_of :zipcode, with: /\A\d{5}-\d{4}|\A\d{5}\z/, message:
-  'Should be Valid'
+  'Should be Valid', allow_blank: true
 
   has_many :ties, dependent: :destroy
   has_many :reps, through: :ties
