3abb2f671307d2358b350a9d89892be3211be101
Ian Wright <wright.ianb@gmail.com>
2017-03-16 16:26:07 -0600

    adds basic styles to Rep.js

--- client/components/Rep.js
+++ client/components/Rep.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import RepIndex from './RepIndex';
 import { updateReps } from '../actions/reps';
+import { Link } from 'react-router';
 
 class Rep extends React.Component {
 
@@ -13,15 +14,47 @@ class Rep extends React.Component {
     let { rep } = this.props;
     if(rep) {
       return(
-        <li className="collection-item avatar">
-          <span className="title">{`${rep.first_name} ${rep.last_name}`}</span>
-          <p>
-            {`${rep.title} of ${rep.state}`}
-          </p>
-          <p>
-            I contain a bio from the sexy API!
-          </p>
-        </li>
+        <div>
+          <div className="center">
+            <h4>{`${rep.first_name} ${rep.last_name}`}</h4>
+            <p>
+              {`${rep.party} ${rep.title} of ${rep.state}`}
+            </p>
+            <p>
+              Next Election: {`${rep.next_election}`} <br />
+            </p>
+          </div>
+          <h5>Contact Info:</h5>
+          <div className="row">
+            <div className="col s12 m6 l4">
+              <ul className="collection">
+                <li className="collection-item">
+                  <i className="fa fa-phone"></i>
+                  {`   ${rep.phone}`}
+                </li>
+                <li className="collection-item">
+                  <i className="fa fa-twitter"></i>
+                  <a
+                    href={`https://twitter.com/${rep.twitter_account}`}
+                    target="_blank"
+                  >
+                    {`   Twitter`}
+                  </a>
+                </li>
+                <li className="collection-item">
+                  <i className="fa fa-external-link"></i>
+                  <a
+                    href={rep.url}
+                    target="_blank"
+                  >
+                  {`   Website`}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Link to="/reps">All My Reps</Link>
+        </div>
       );
     } else {
       return null;
