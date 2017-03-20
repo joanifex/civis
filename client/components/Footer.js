eed041bd5c35efeb4644db36970788740dd034eb
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-03-20 09:42:42 -0600

    adds Footer and ParallaxContainer components

---
+++ client/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { footer, footerText } from './styles.scss';
+
+const Footer = () => (
+  <div>
+    <footer className='blue-grey white-text'>
+      <div className="footer-copyright">
+        <div className="container">
+        © 2017 Civis
+        <div className= {`${footerText} right`} style={{ verticalAlign: 'middle' }}>
+        <Link to='/about'>About</Link>
+        </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+)
+
+export default Footer;
--- client/components/Home.js
+++ client/components/Home.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import { browserHistory, Link } from 'react-router';
 import HomeCard from './HomeCard';
 import AboutUs from './AboutUs';
-import { homeImgFirst, homeImgSecond, section, footer, footerText } from './styles.scss';
+import Footer from './Footer';
+import ParallaxContainer from './ParallaxContainer';
+import { s } from './styles.scss';
 
 class Home extends React.Component {
 
@@ -14,47 +16,25 @@ class Home extends React.Component {
   render() {
     return(
       <div>
-        <div className="parallax-container">
-          <div className="parallax">
-            <img className={homeImgFirst}
-            src="https://c.tribune.com.pk/2016/12/1252658-protests-1480799399-425-160x120.jpg"
-            />
-            <div>
-              <h2 className='center'>Civis</h2>
-              <HomeCard />
-            </div>
-          </div>
-        </div>
-        <div className={`${section} white`}>
+        <h2 className='center'>Civis</h2>
+        <HomeCard />
+        <ParallaxContainer />
+
+        <div className='section white'>
           <div className="row container">
             <h2 className="header">Parallax</h2>
             <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
           </div>
         </div>
-        <div className={`${section} white`}>
+        <div className='section white'>
           <div className="row">
             <div><AboutUs /></div>
           </div>
         </div>
-        <div className="parallax-container">
-          <div className="parallax">
-            <img className={homeImgSecond}
-            src="https://c.tribune.com.pk/2016/12/1252658-protests-1480799399-425-160x120.jpg"
-            />
-          </div>
-        </div>
 
-        <footer className='blue-grey white-text'>
-          <div className="footer-copyright">
-            <div className="container">
-            © 2017 Civis
-            <div className= {`${footerText} right`} style={{ verticalAlign: 'middle' }}>
-            <Link to='/about'>About</Link>
-            </div>
-            </div>
-          </div>
-        </footer>
-      </div>
+       <ParallaxContainer />
+       <Footer />
+     </div>
     )
   }
 };
