167f8e9f5580112bef646443f80291131b445999
Ian Wright <wright.ianb@gmail.com>
2017-03-21 14:14:13 -0600

    stubs out users_controller_spec

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -26,6 +26,18 @@ RSpec.describe Api::UsersController, type: :controller do
     context 'logged in user' do
       login_user
 
+      it 'returns http success' do
+        get :user_reps, format: :json
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'calls the sets_reps_pictures method on the current user' do
+        # expect the users reps to not have profile_urls
+        # get :user_reps, format: :json
+        # expect the users reps to have profile_urls
+        binding.pry
+      end
+
       it 'render JSON of the users reps' do
         get :user_reps, format: :json
         parsed = JSON.parse(response.body)
@@ -34,7 +46,25 @@ RSpec.describe Api::UsersController, type: :controller do
         expect(reps.count).to eq(@user.reps.count)
       end
     end
+  end
 
-    
+  describe 'PATCH #update_zipcode' do
+    it 'returns http success' do
+    end
+
+    it 'finds the users location by zipcode' do
+    end
+
+    it 'renders json of errors if location is nil' do
+    end
+
+    it 'finds the state based upon the location' do
+    end
+
+    it 'updates the users zipcode' do
+    end
+
+    it 'renders json of errors if zipcode is invalid' do
+    end
   end
 end
