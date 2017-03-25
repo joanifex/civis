a0539306bfbda32b088e1b65bcc311eaf7189e3c
Ian Wright <wright.ianb@gmail.com>
2017-03-25 09:32:53 -0600

    adds geolocation, but on client side

--- client/components/AddressForm.js
+++ client/components/AddressForm.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 
-// TODO: refactor into presentational component
 class AddressForm extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
     let address = this.address.value
+    this.updateAddress(address)
+  }
+
+  geolocate = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( position => {
+        let coords = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log(coords);
+        let url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+        let key = 'AIzaSyCk8j46oOc98pfId6TdgvjgDqCcEkJxB40'
+        $.ajax({
+          url: `${url}latlng=${coords.lat},${coords.lng}&key=${key}`,
+          type: 'GET'
+        }).done( data => {
+          let address = data.results[0].formatted_address;
+          this.updateAddress(address);
+        }).fail( err => {
+          Materialize.toast('Error');
+        });
+      });
+    } else {
+      Materialize.toast("Can't Geolocate.")
+    }
+  }
+
+  updateAddress = (address) => {
     $.ajax({
       type: 'PATCH',
-      url: '/api/user/address',
+      url: `/api/user/address`,
       dataType: 'JSON',
       data: { address }
     }).done(data => {
@@ -39,6 +67,7 @@ class AddressForm extends React.Component {
             <input className='btn blue-grey' type='submit'/>
           </div>
         </form>
+        <button className="btn" onClick={this.geolocate}>Geolocate</button>
       </div>
     );
   }
