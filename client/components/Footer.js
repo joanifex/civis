436ce2825f5a7b498eb35c30851cc991c0bdf8b3
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-04-04 14:52:49 -0600

    makes footer links open in new tab

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -9,21 +9,26 @@ const Footer = () => (
         <h5 className="white-text">APIs</h5>
         <div className='row' style={{marginBottom: '0', paddingBottom: '30px'}}>
           <a className='offset-m1 white-text col s12 m2'  href="https://www.propublica.org/datastore/api/propublica-congress-api"
+          target='_blank'
           >ProPublica
           </a>
           <a className='white-text col s12 m2'
           href="https://developer.nytimes.com/"
+          target='_blank'
           >New York Times
           </a>
           <a className='white-text col s12 m2'  href="https://www.mediawiki.org/wiki/API:Main_page"
+          target='_blank'
           >Wikipedia
           </a>
           <a className='white-text col s12 m2'
           href="https://dev.twitter.com/rest/public"
+          target='_blank'
           >Twitter
           </a>
           <a className='white-text col s12 m2'
           href="https://developers.google.com/maps/"
+          target='_blank'
           >Google Maps
           </a>
         </div>
